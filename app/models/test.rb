1fd98a9964989da1af93b8df75f3ce5a789fdaac
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-06-04 14:28:09 +0300

    Add assoc, change methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,12 @@
 class Test < ApplicationRecord
   belongs_to :category
-  belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User', inverse_of: :tests
+
+  has_many :passed_tests, dependent: :destroy
+  has_many :users, through: :passed_tests
+  has_many :questions, dependent: :destroy
 
   def self.names_by_category(name)
-    joins('JOIN categories ON tests.category_id = categories.id')
-      .where(categories: { title: name })
-      .order(title: :desk)
-      .pluck(:title)
+    joins(:category).where(categories: { title: name }).order(:title).pluck(:title)
   end
 end
--- db/migrate/20210601105631_create_passed_tests.rb
+++ db/migrate/20210601105631_create_passed_tests.rb
@@ -1,8 +1,8 @@
 class CreatePassedTests < ActiveRecord::Migration[6.1]
   def change
     create_table :passed_tests do |t|
-      t.references :test, null: false, foreign_key: true
-      t.references :user, null: false, foreign_key: true
+      t.references :test, null: false, foreign_key: true, unique: true
+      t.references :user, null: false, foreign_key: true, unique: true
 
       t.timestamps
     end
