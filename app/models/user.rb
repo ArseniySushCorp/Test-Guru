4197f31377aa122b5c45cfd9b76120376d3d948f
Arseniy Suschevskiy <58954499+ArseniySushCorp@users.noreply.github.com>
2021-06-06 13:41:14 +0300

    Add assoc, change methods (#4)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  has_many :passed_tests, dependent: :destroy
+  has_many :tests, through: :passed_tests
+
   def tests_by_level(level)
-    Test
-      .joins('LEFT JOIN passed_tests ON tests.id = passed_tests.test_id')
-      .where(passed_tests: { user_id: id }, level: level)
+    tests.where(level: level)
   end
 end
