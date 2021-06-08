3764379bdf0cee3ce7ef0035f50b802d759b962e
Arseniy Suschevskiy <58954499+ArseniySushCorp@users.noreply.github.com>
2021-06-08 16:25:43 +0300

    Homework 5 / Add validates and scopes (#5)
    * Add validates and scopes
    
    * change scope names_by_category to method, change validates and deafult_scope order
    
    * Little changes in scope and method name

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,13 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :text, presence: true
+  validates :correct, presence: true
+  validate :answers_amount, on: :create
+
+  scope :correct, -> { where(correct: true) }
+
+  def answers_amount
+    errors.add(:answer, 'too much answers') if question.answers.size >= 4
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  validates :login, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+
+  has_many :own_tests, class_name: 'Test', foreign_key: :author_id, inverse_of: :author, dependent: :destroy
   has_many :passed_tests, dependent: :destroy
   has_many :tests, through: :passed_tests
-
-  def tests_by_level(level)
-    tests.where(level: level)
-  end
 end
