565423c9e6dec1b7de76f645f92724cd066e0f3a
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-06-10 02:06:13 +0300

    Add question controller, question methods - index, create, destroy, new

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,43 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_not_found
+
+  def show; end
+
+  def new; end
+
+  def index
+    @questions = @test.questions
+  end
+
+  def create
+    @question = @test.questions.new(text: params.require(:question_text))
+
+    if @question.save
+      render plain: "Qustion ##{@question.id} created"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy
+
+    render plain: "Question #{@question.id} deleted"
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params.require(:test_id))
+  end
+
+  def find_question
+    @question = Question.find(params.require(:id))
+  end
+
+  def rescue_with_not_found
+    render plain: 'Test not found'
+  end
+end
