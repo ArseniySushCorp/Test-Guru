cbd4dcb0adf6a9e376c2889b9fe407765a86ef35
Arseniy Suschevskiy <58954499+ArseniySushCorp@users.noreply.github.com>
2021-06-15 20:36:36 +0300

    Homework 6 (#6)
    * Add question controller, question methods - index, create, destroy, new
    
    * Change title, add delete to view
    
    * correct question path
    
    * fix params on new question
    
    * Change create method, changes in form
    
    * correct create method

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,49 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_not_found
+
+  def show; end
+
+  def new
+    @question = Question.new
+  end
+
+  def index
+    @questions = @test.questions
+  end
+
+  def create
+    @question = @test.questions.create(question_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @question.destroy
+
+    render plain: "Question #{@question.id} deleted"
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:text)
+  end
+
+  def find_test
+    @test = Test.find(params.require(:test_id))
+  end
+
+  def find_question
+    @question = Question.find(params.require(:id))
+  end
+
+  def rescue_with_not_found
+    render plain: 'Test not found'
+  end
+end
