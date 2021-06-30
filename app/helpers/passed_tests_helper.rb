1ec245ef4e8ab8d558afc5edb706f0715cbdae40
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-07-01 02:24:16 +0300

    Complete homework 8, send to review

--- app/helpers/passed_tests_helper.rb
+++ app/helpers/passed_tests_helper.rb
@@ -1,2 +1,17 @@
 module PassedTestsHelper
+  def score(passed_test)
+    percents = to_percents(passed_test.result)
+
+    tag.span percents, class: passed_test.success? ? 'green-text' : 'red-text'
+  end
+
+  def closing_message(passed_test)
+    tag.p "Test #{passed_test.success? ? 'passed successfully' : 'failed'}"
+  end
+
+  private
+
+  def to_percents(number)
+    "#{(number * 100).round}%"
+  end
 end
--- app/models/passed_test.rb
+++ app/models/passed_test.rb
@@ -3,12 +3,11 @@ class PassedTest < ApplicationRecord
   belongs_to :user
   belongs_to :current_question, class_name: 'Question', optional: true, inverse_of: :passed_tests
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :set_current_question, on: %i[create update]
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
 
-    self.current_question = next_question
     save!
   end
 
@@ -16,10 +15,22 @@ class PassedTest < ApplicationRecord
     current_question.nil?
   end
 
+  def result
+    correct_questions / test.questions.size.to_f
+  end
+
+  def success?
+    result > 0.85
+  end
+
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def set_current_question
+    self.current_question = current_question.nil? ? test.questions.first : next_question
+  end
+
+  def before_save_set_next_question
+    self.current_question = next_question
   end
 
   def correct_answer?(answer_ids)
@@ -33,6 +44,6 @@ class PassedTest < ApplicationRecord
   end
 
   def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+    test.questions.order(:id).find_by('id > ?', current_question.id)
   end
 end
