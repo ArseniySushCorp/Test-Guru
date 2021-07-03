76037a931d60c8e94edf9b506b0f21c2146cc7ca
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-07-03 21:44:47 +0300

    Add sessions, complete user auth, add shared/errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,11 @@
 class User < ApplicationRecord
-  validates :login, presence: true
-  validates :email, presence: true
-  validates :password_digest, presence: true
-
   has_many :own_tests, class_name: 'Test', foreign_key: :author_id, inverse_of: :author, dependent: :destroy
   has_many :passed_tests, dependent: :destroy
   has_many :tests, through: :passed_tests
 
-  def authenticate(password_string)
-    digest(password_string) == password_digest ? self : false
-  end
+  has_secure_password
 
   def passed_test(test)
     passed_tests.order(id: :desc).find_by(test_id: test.id)
-  end
-
-  private
-
-  def digest(string)
-    Digest::SHA1.hexdigest(string)
   end
 end
