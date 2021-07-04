2604a2f31ccc6293b319199fa5b6a4b0e016c322
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-07-04 22:37:44 +0300

    Add session

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,5 @@
 class TestsController < ApplicationController
+  before_action :authenticate_user!
   before_action :find_test, only: %i[show edit start update destroy]
   before_action :find_user, only: %i[start]
 
