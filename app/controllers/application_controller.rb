b3f146085e179d7f76a8312ddce1f43c3e87cd8b
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-08-01 05:15:51 +0300

    Add sessions controller from devise, correct view and add action for flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,6 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  def after_sign_up_path_for(user)
-    if current_user.sign_in_count == 1
-      flash[:notice] = t('signed_up', name: user.first_name)
-    end
-  end
-
   def after_sign_in_path_for(user)
     if user.is_a?(Admin)
       admin_tests_path
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,13 @@
+class SessionsController < Devise::SessionsController
+  after_action :greetings_after_login, only: :create
+
+  def create
+    super
+  end
+
+  private
+
+  def greetings_after_login
+    flash[:notice] = current_user.sign_in_count == 1 ? t('signed_up', name: current_user.first_name) : t(signed_in)
+  end
+end
